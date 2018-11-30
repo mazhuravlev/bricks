5282ea648b4e617610c2007a3f5564d53ff91584
Mihail <freon2006@bk.ru>
2018-11-30 14:37:47 +0500

    grid snap

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Bricks editor application</h1>
-        </header>
         <div className="appContainer">
           <Editor />
         </div>
--- src/components/GridBricks.jsx
+++ src/components/GridBricks.jsx
@@ -1,20 +1,49 @@
 import React, { Component } from 'react'
 
+const getGridPos = (x, y, step) => {
+  const posX = Math.floor(x / step);
+  const posY = Math.floor(y / step);
+  return { x: posX, y: posY, step };
+};
+
+const buildMerkerStyle = ({ x, y, step }) => ({
+  left: x * step,
+  top: y * step,
+});
+
 export class GridBricks extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      step: 15,
+    };
   }
 
   render() {
+    const {
+      // isPositionOutside = false,
+      elementDimensions: {
+        width = 0,
+        height = 0
+      } = {},
+      position: {
+        x = 0,
+        y = 0
+      } = {}
+    } = this.props;
+
+    const gridPos = getGridPos(x, y, this.state.step);
+
+    console.log(`Element width: ${width} height: ${height}`);
+    console.log(`Cursor position! X: ${x} Y: ${y}`);
+    console.log(`Grid position! X: ${gridPos.x} Y: ${gridPos.y}`);
     return (
       <div className="bricks-grid">
+        <div className='marker-grid-position' style={buildMerkerStyle(gridPos)}></div>
         <div className="testbrick"></div>
-        <p className="grid-test-lebel">Element width: {this.props.elementDimensions.width} height: {this.props.elementDimensions.height}</p>
-        <p className="grid-test-lebel">Cursor position! X: {this.props.position.x} Y: {this.props.position.y}</p>
       </div>
     )
   }
 }
 
-export default GridBricks
+export default GridBricks;
