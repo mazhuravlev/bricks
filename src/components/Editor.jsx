2fb92d6d3b462f6316a79acc897098ed9c8f9818
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2018-11-30 12:30:25 +0300

    операции в редакторе

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import cn from 'classnames';
 import ReactCursorPosition from 'react-cursor-position';
 
 import GridBricks from './GridBricks';
+import { ADD_BRICK } from '../operations';
 
 export class Editor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectBrick: {
-        size: 1,
-        orientation: 'horizontal',
-      },
+      operation: undefined
     };
   }
 
@@ -19,30 +16,29 @@ export class Editor extends Component {
     this.setState({ selectBrick: { ...selectBrick } });
   }
 
+  setOperation(operation) {
+    this.setState({ operation });
+  }
+
+  setBrickOperation = (x, y) => () => this.setOperation({ type: ADD_BRICK, data: { x, y } })
+
   render() {
-    const { size, orientation } = this.state.selectBrick;
     return (
       <div>
         <div className="toolsPanel">
-          <p>Select horizontal brick:</p>
           <div className="btn-group bricks-horizontal-size">
-            <button onClick={this.changeSize({ size: 1, orientation: 'horizontal' })}type="button" className={cn({ btn: true, active: (size === 1 && orientation === 'horizontal') })}>1</button>
-            <button onClick={this.changeSize({ size: 0.75, orientation: 'horizontal' })}type="button" className={cn({ btn: true, active: (size === 0.75 && orientation === 'horizontal') })}>0,75</button>
-            <button onClick={this.changeSize({ size: 0.5, orientation: 'horizontal' })}type="button" className={cn({ btn: true, active: (size === 0.5 && orientation === 'horizontal') })}>0,5</button>
-            <button onClick={this.changeSize({ size: 0.25, orientation: 'horizontal' })}type="button" className={cn({ btn: true, active: (size === 0.25 && orientation === 'horizontal') })}>0,25</button>
+            <button onClick={this.setBrickOperation(4, 1)}>4x1</button>
+            <button onClick={this.setBrickOperation(3, 1)}>3x1</button>
+            <button onClick={this.setBrickOperation(2, 1)}>2x1</button>
+            <button onClick={this.setBrickOperation(1, 1)}>1x1</button>
           </div>
-          <p>Select vertical brick:</p>
           <div className="btn-group bricks-vertical-size">
-            <button onClick={this.changeSize({ size: 1, orientation: 'vertival' })} type="button" className={cn({ btn: true, active: (size === 1 && orientation === 'vertival') })}>1</button>
-            <button onClick={this.changeSize({ size: 0.75, orientation: 'vertival' })}type="button" className={cn({ btn: true, active: (size === 0.75 && orientation === 'vertival') })}>0,75</button>
-            <button onClick={this.changeSize({ size: 0.5, orientation: 'vertival' })}type="button" className={cn({ btn: true, active: (size === 0.5 && orientation === 'vertival') })}>0,5</button>
-            <button onClick={this.changeSize({ size: 0.25, orientation: 'vertival' })}type="button" className={cn({ btn: true, active: (size === 0.25 && orientation === 'vertival') })}>0,25</button>
+            <button onClick={this.setBrickOperation(1, 4)}>1x4</button>
+            <button onClick={this.setBrickOperation(1, 3)}>1x3</button>
+            <button onClick={this.setBrickOperation(1, 2)}>1x2</button>
           </div>
         </div>
         <div className="workArea">
-          <h2>Grid</h2>
-          <p>size: {this.state.selectBrick.size}</p>
-          <p>orientation: {this.state.selectBrick.orientation}</p>
           <ReactCursorPosition>
             <GridBricks />
           </ReactCursorPosition>
---
+++ src/operations.js
@@ -0,0 +1,1 @@
+export const ADD_BRICK = 'ADD_BRICK';
