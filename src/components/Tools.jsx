c46022db0493ad5f7923cb080ed7c1a943d97ce9
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2018-12-05 10:28:34 +0300

    исправил размер png

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import domtoimage from 'dom-to-image';
 
 export default class Tools extends Component {
   constructor(props) {
